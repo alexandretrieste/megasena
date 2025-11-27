dd51b445924b88f48f6a33bb2d49e25d3299b9f4
Alexandre <alexandre.trieste@gmail.com>
2025-11-27 14:03:04 -0300

    Primeira versão do simulador Mega-Sena

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const express = require('express');
 const { registerVolante, getStats } = require('./lib/volanteService');
 
 const app = express();
